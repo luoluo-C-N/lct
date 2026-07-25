d9f9dad96777102486b7967749e2ec801f164ed9
彦洛_yanluo <524609769@qq.com>
2026-07-26 02:46:09 +0800

    迁移 magic-image-library

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      ignored: [
+        '**/src-tauri/target/**',
+        '**/src-tauri/gen/**',
+        '**/*.exe'
+      ]
+    }
+  },
   test: {
     environment: 'jsdom',
     globals: true
